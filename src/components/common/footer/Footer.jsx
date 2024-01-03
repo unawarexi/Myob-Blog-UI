816b4bd3eb3731ca8a70ba424495be3392740db0
unawarexi <unawarexi@gmail.com>
2024-01-03 07:21:41 +0100

    Updated css

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -60,7 +60,7 @@ const Footer = () => {
         <div className='container flexSB'>
           <p>© all rights reserved</p>
           <p>
-            made with <i className='fa fa-heart'></i> by gorkhcoder
+            made with <i className='fa fa-heart'></i> by Oligarch Corps
           </p>
         </div>
       </div>
